9b4f0238c91dff78ca9ebf2f539fea629986d6bc
Ricardo <sp.ricardo@gmail.com>
2025-12-07 22:36:12 -0300

    FIX

--- angular/projeto1/conceitos-basicos/src/app/helloworld/helloworld.component.ts
+++ angular/projeto1/conceitos-basicos/src/app/helloworld/helloworld.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 // @component é um decorator que altera o comportamento da classe abaixo dele
 @Component({
     selector: 'hello-world-ricardo', // nome da tag HTML que representa esse componente
-    template: '<h1>Hello World Component!</h1>', // template HTML do componente
+    template: '<h1>Hello World Component 2!</h1>', // template HTML do componente
 })
 
 export class HelloworldComponent {
