0793831dac9e217218c589d7019ad20ebe7022ac
Ricardo <sp.ricardo@gmail.com>
2025-12-07 22:32:19 -0300

    continuacao

--- angular/projeto1/conceitos-basicos/src/app/app.component.ts
+++ angular/projeto1/conceitos-basicos/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloworldComponent } from './helloworld/helloworld.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelloworldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ angular/projeto1/conceitos-basicos/src/app/helloworld/helloworld.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'; 
+
+// @component é um decorator que altera o comportamento da classe abaixo dele
+@Component({
+    selector: 'hello-world-ricardo', // nome da tag HTML que representa esse componente
+    template: '<h1>Hello World Component!</h1>', // template HTML do componente
+})
+
+export class HelloworldComponent {
+}
