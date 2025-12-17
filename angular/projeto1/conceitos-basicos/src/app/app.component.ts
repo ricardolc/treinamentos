7ec7821d009cb3ff7ef22a83c4c63957026e0be9
Ricardo <sp.ricardo@gmail.com>
2025-12-17 13:03:26 -0300

    calculadora

--- angular/projeto1/conceitos-basicos/src/app/app.component.ts
+++ angular/projeto1/conceitos-basicos/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloworldComponent } from './helloworld/helloworld.component';
+import {MinhapaginaComponent} from './minhapagina/minhapagina.component';
+import {CalculadoraComponent} from './calculadora/calculadora.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloworldComponent],
+  imports: [HelloworldComponent, MinhapaginaComponent, CalculadoraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
